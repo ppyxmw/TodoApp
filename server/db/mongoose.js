532302e182108927b279a474910575a5e6597515
EC2 Default User <ec2-user@ip-172-31-34-74.eu-west-1.compute.internal>
2018-05-15 02:28:33 +0000

    set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 const MONGO_URL = 'mongodb://ppyxmw:PADrydVoipsich4@ds217560.mlab.com:17560/781911583413';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(process.env.MONGODB_URI || MONGO_URL);
 
 module.exports = {
  mongoose   
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
  
@@ -48,8 +49,8 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(8080, () => {
-  console.log('Started on port 8080.');
+app.listen(port, () => {
+  console.log(`Started on port {port}.`);
 });
 
 module.exports = {app};
