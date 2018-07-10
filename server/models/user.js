4eb2060e3380e6b7e6f666daf00b30c592cb02f2
Michael <ppyxmw@gmail.com>
2018-07-10 08:55:39 +0000

    hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -72,22 +72,20 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
-// UserSchema.pre('save', function (next) {
-//   var user = this;
+UserSchema.pre('save', function (next) {
+  var user = this;
   
-//   if(user.isModified('password')) {
-//     bcrypt.genSalt(10, (err, salt) => {
-//       bcrypt.hash(user.password, salt, (err, hash) => {
-//         user.password = hash;
-//         next();
-//       });
-//     });
-    
-//   } else {
-    
-//   }
-
-// })
+  if(user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
