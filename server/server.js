d3f4568c9259eb8b4681ec2d8fde3d5587d87656
Michael <ppyxmw@gmail.com>
2018-07-16 02:45:56 +0000

    adds POST /users/login route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const {ObjectID} = require("mongodb");
 
 
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -108,17 +109,20 @@ app.post('/users', (req, res) => {
   });
 });
 
-// app.get('/users', (req, res) => {
-//   User.find().then((users) => {
-//     res.send({users});
-//   }, (e) => {
-//     res.status(400).send(e);  
-//   });
-// });
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    return res.status(400).send();
+  });
+});
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
-  
 });
 
 app.listen(port, () => {
